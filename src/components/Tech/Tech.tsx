88ea58c006baf0737a4b8ed8407e9b2afb3ce80f
Carl <sprintman210@gmail.com>
2022-12-16 12:35:42 -0500

    every day tech update

--- src/components/Tech/Tech.tsx
+++ src/components/Tech/Tech.tsx
@@ -1,11 +1,16 @@
 import { Container, Box, Flex, Heading, Image } from "@chakra-ui/react";
+import { Layout } from "../Layout/Layout";
 
 const Tech = () => {
   return (
-    <Container>
-      <Heading>Every Day Tech</Heading>
-      <Image src="/carl.jpg" alt="CaAWL" width="30rem"></Image>
-    </Container>
+    <Layout>
+      <Flex>
+        <Heading>Every Day Tech</Heading>
+        <Heading size="md">What I Use To Work</Heading>
+
+        <Heading size="md">What I Use To Play</Heading>
+      </Flex>
+    </Layout>
   );
 };
 export default Tech;
