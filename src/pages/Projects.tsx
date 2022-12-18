b6862509df8c81f536125f88428fddd57450b5e3
Carl <sprintman210@gmail.com>
2022-12-18 10:03:16 -0500

    project page update

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -1,14 +1,24 @@
-import { Heading, Box, Text, Spacer } from "@chakra-ui/react";
+import { Heading, Box, Text, Center, Flex } from "@chakra-ui/react";
 import { Layout } from "../components/Layout/Layout";
 
-const Projects = ({}) => {
+const Projects = () => {
   return (
     <Layout>
-      <Heading>Projects</Heading>
-      <Spacer />
-      <Box>
-        <Text>Client Film Review Platform</Text>
-      </Box>
+      <Center>
+        <Heading>Projects</Heading>
+      </Center>
+      <Flex justify="center" pt="12" direction="column">
+        <Box>
+          <Center>
+            <Text>Client Film Review Platform</Text>
+          </Center>
+        </Box>
+        <Box pt="2">
+          <Center>
+            <Text>Spotify Stats</Text>
+          </Center>
+        </Box>
+      </Flex>
     </Layout>
   );
 };
