ffe381a3a0df2145bbe4e42e842b25409e0759be
Carl <sprintman210@gmail.com>
2022-12-19 19:56:29 -0500

    tech page update, chakra style props experimentation

--- src/components/Tech/Tech.tsx
+++ src/components/Tech/Tech.tsx
@@ -1,14 +1,43 @@
-import { Container, Box, Flex, Heading, Image } from "@chakra-ui/react";
+import {
+  Box,
+  Flex,
+  Heading,
+  Center,
+  List,
+  ListItem,
+  Spacer,
+} from "@chakra-ui/react";
 import { Layout } from "../Layout/Layout";
 
 const Tech = () => {
   return (
     <Layout>
-      <Flex>
-        <Heading>Every Day Tech</Heading>
-        <Heading size="md">What I Use To Work</Heading>
-
-        <Heading size="md">What I Use To Play</Heading>
+      <Flex direction="column">
+        <Center>
+          <Heading>Every Day Tech</Heading>
+        </Center>
+        <Box mt="10">
+          <Heading size="md">What I Use To Work</Heading>
+          <Spacer />
+          <List mt="5">
+            <ListItem>Macbook Pro 2016 </ListItem>
+            <ListItem>VS Code</ListItem>
+            <ListItem>MX Master 3S</ListItem>
+          </List>
+        </Box>
+        <Box mt="10">
+          <Heading size="md">What I Use To Play</Heading>
+          <Spacer />
+          <List mt="5">
+            <ListItem>
+              Lian Li O11 Dynamic EVO Tempered Glass ATX Mid-Tower Computer Case
+            </ListItem>
+            <ListItem>
+              Gigabyte NVIDIA GeForce RTX 3080 Ti VISION Overclocked Triple-Fan
+              12GB
+            </ListItem>
+          </List>
+        </Box>
       </Flex>
     </Layout>
   );
