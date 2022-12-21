5fe33f1db6c2295761d27bd8436eb2b275c488f4
Carl <sprintman210@gmail.com>
2022-12-21 15:59:16 -0500

    modify social items

--- src/components/Socials/SocialItems.ts
+++ src/components/Socials/SocialItems.ts
@@ -1,10 +1,9 @@
-import React from "react";
 import { FaGithub, FaInstagram, FaLinkedin } from "react-icons/fa";
 import { IconType } from "react-icons/lib";
 
 interface SocialItem {
-  icon:IconType;
-    alt: string;
+  icon: IconType;
+  alt: string;
   href: string;
 }
 
@@ -12,16 +11,16 @@ export const SocialItems: SocialItem[] = [
   {
     icon: FaGithub,
     href: "https://github.com/CarlinCharge",
-    alt:"Github Profile"
+    alt: "Github Profile",
   },
-    {
+  {
     icon: FaInstagram,
     href: "https://www.instagram.com/carlconroydesign/",
-    alt:"Github Profile"
+    alt: "Github Profile",
   },
-    {
+  {
     icon: FaLinkedin,
     href: "https://www.instagram.com/carlconroydesign/",
-    alt:"Github Profile"
-  }
+    alt: "Github Profile",
+  },
 ];
