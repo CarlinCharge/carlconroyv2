ffb0572ad694f1988ceee0862b1b71839df6178c
Carl <sprintman210@gmail.com>
2022-12-23 09:47:48 -0500

    fix about page content layout

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -5,14 +5,14 @@ import { Bio } from "./Bio";
 const About = () => {
   return (
     <Container maxW="6xl">
-      <Flex align="center" justify="center">
+      <Flex justify="center">
         <Heading width="50%" textAlign="center">
           Carl Conroy is a visual storyteller, thoughtful UX designer, and
           software developer.
         </Heading>
       </Flex>
-      <Flex gap="10" pt="10">
-        <Box>
+      <Flex gap="10" pt="10" justifyContent="center">
+        <Box maxW="sm">
           <Heading size="md">Outside Work</Heading>
           <Bio />
         </Box>
