1ba7e1c609d57a8db5f15acd01bd2273c73e71a8
Carl <sprintman210@gmail.com>
2022-12-24 12:56:20 -0500

    socials useMediaquery needs work and experimentation

--- src/components/Socials/StickySocials.tsx
+++ src/components/Socials/StickySocials.tsx
@@ -1,10 +1,24 @@
-import { Icon, Grid, List, ListItem, Link } from "@chakra-ui/react";
+import { Icon, useMediaQuery, List, ListItem, Link } from "@chakra-ui/react";
+import { NodeNextRequest } from "next/dist/server/base-http/node";
 import { SocialItems } from "./SocialItems";
 
 const size = "10";
+
 export const StickySocials = () => {
+  const [isSmallerThan850] = useMediaQuery([
+    `.socials {
+      display: none; 
+    }`,
+  ]);
   return (
-    <List position="fixed" bottom="20" gap="6" display="grid">
+    <List
+      position="fixed"
+      bottom="20"
+      gap="6"
+      display="grid"
+      className="socials"
+    >
+      {isSmallerThan850 ? "smaller than 850" : "larger than 850"}
       {SocialItems.map((item) => {
         return (
           <ListItem>
