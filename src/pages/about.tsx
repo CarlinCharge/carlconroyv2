83b72befd88f932520189dbbbe23f336378303d0
Carl Conroy <sprintman210@gmail.com>
2023-01-25 09:09:12 -0500

    about fix

---
+++ src/pages/about.tsx
@@ -0,0 +1,13 @@
+import { Container, Box } from "@chakra-ui/react";
+import { Layout } from "../components/Layout/Layout";
+import About from "../components/About/About";
+
+const AboutPage = () => {
+  return (
+    <Layout>
+      <About />
+    </Layout>
+  );
+};
+
+export default AboutPage;
