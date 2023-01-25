28c097f21b91ab4e85f98a3ee148df7b05386d74
Carl Conroy <sprintman210@gmail.com>
2023-01-25 09:37:16 -0500

    adds fonts

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,26 @@
 import type { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
 import { theme } from "../styles/theme";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider theme={theme}>
+      <Head>
+        <title>Carl Conroy</title>
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="anonymous"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+
       <Component {...pageProps} />
     </ChakraProvider>
   );
