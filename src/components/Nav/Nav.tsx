c6aa965f49f2d28831b367ac25723af7d3b1c48b
Carl Conroy <sprintman210@gmail.com>
2023-06-10 15:08:41 -0400

    nav font size

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -3,15 +3,17 @@ import { Link, Flex } from "@chakra-ui/react";
 import { MenuItems } from "./MenuItems";
 
 const Nav = () => {
+  const fontSize = "26.45px";
   return (
     <Flex justifyContent="space-between" alignItems="center">
-      <Link as={NextLink} href="/">
+      <Link as={NextLink} href="/" fontSize={fontSize}>
         Carl Conroy
       </Link>
       <Flex>
         {MenuItems.map((item) => {
           return (
             <Link
+              fontSize={fontSize}
               as={NextLink}
               href={item.href}
               key={item.text}
