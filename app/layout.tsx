3935cd0caa79102d94d4cb009a0858d276261236
Carl Conroy <sprintman210@gmail.com>
2023-06-12 05:55:07 -0400

    load font via head

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,23 +6,13 @@ import { theme } from "./../src/styles/theme";
 import Nav from "../src/components/Nav/Nav";
 import { StickySocials } from "../src/components/Socials/StickySocials";
 
-import { Montserrat } from "@next/font/google";
-
-const montserrat = Montserrat({
-  // Specifying weight is only required for
-  // non-variable fonts.
-  weight: "400",
-  subsets: ["latin"],
-  display: "swap",
-});
-
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" className={montserrat.className}>
+    <html lang="en">
       <body>
         <ChakraProvider theme={theme}>
           <Container maxW={{ base: "5xl", md: "40px", lg: "6xl" }}>
